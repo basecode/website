37b84c9c43733e1bda9c43f89516c7a57c901e40
Tobias Reiss <tag+github@basecode.de>
2014-03-19 00:07:50 +0100

    Move tracking to main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+(function tracking(doc) {
+  if (doc.location.hostname === 'localhost')
+    return;
+
+  var gaJsHost = (("https:" == doc.location.protocol) ? "https://ssl." : "http://www.");
+  doc.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+})(document);
+
+try {
+  var pageTracker = _gat._getTracker("UA-39978115-1");
+  pageTracker._trackPageview();
+} catch(err) {}
